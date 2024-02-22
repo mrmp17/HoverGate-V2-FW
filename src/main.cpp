12fb3fec5fce6bf02c159eacdeedfb106caedfa0
Matej Planinsek <matej.planinsek@gmail.com>
2024-02-22 19:13:57 +0100

    Added ESP-NOW comms sutff. Not finished, not tested.

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,15 @@
 #include "BLDC_driver.h"
 #include "latch.h"
 #include "gate.h"
+#include <WiFi.h>
+#include <esp_now.h>
+#include "secrets.h"
 
-#define axupin 11
+// #define axupin 11
 
+// short gate is master (has RF, implements MQTT, commands long)
+
+// select gate. define GATE_SHORT for short gate, comment out for long gate
 #define GATE_SHORT // comment if compiling for long gate wing
 
 #ifdef GATE_SHORT
@@ -60,15 +66,70 @@ gate_params params {
 Latch latch;
 #endif
 
+// typedefs for ESP-NOW messages
+typedef struct MsgShortToLong {
+    uint8_t action_cmd;
+} t_msg_short_to_long;
+
+typedef struct MsgLongToShort {
+    uint8_t gate_state;
+    uint8_t error_code;
+    float gate_angle;
+    float bat_volt;
+} t_msg_long_to_short;
+
+//global variables for ESP-NOW comms messages
+t_msg_short_to_long msg_short_to_long;
+t_msg_long_to_short msg_long_to_short;
+uint32_t msg_num_recvd = 0; //to check for missed messages when processing. increments in callback, decrements at processing
+uint32_t msg_deliver_ok = 0; //flag to check if message was sent and delivered
+
+#ifdef GATE_SHORT
+// short gate sends to long gate mac
+uint8_t send_to_mac_addr[] = mac_gate_long;
+#endif
+#ifndef GATE_SHORT
+// long gate sends to short gate mac
+uint8_t send_to_mac_addr[] = mac_gate_short;
+#endif
+
+
+
+
 BLDC_driver BLDC;
 Gate gate(params);
+
+// ESP-NOW recv callback 
+void onEspnowRecv(const uint8_t * mac, const uint8_t *incomingData, int len){
+  //short gate is receiving from long
+  #ifdef GATE_SHORT
+  memcpy(&msg_long_to_short, incomingData, sizeof(msg_long_to_short));
+  #endif
+
+  //long gate is receiving from short
+  #ifndef GATE_SHORT
+  memcpy(&msg_short_to_long, incomingData, sizeof(msg_short_to_long));
+  #endif
+
+  msg_num_recvd++;
+}
+
+// ESP-NOW sent callback
+void onEspnowSent(const uint8_t *mac_addr, esp_now_send_status_t status){
+  if(status == ESP_NOW_SEND_SUCCESS){
+    msg_deliver_ok = 1;
+  }
+  else{
+    msg_deliver_ok = 0;
+  }
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "ESP-NOW packed delivered" : "ESP-NOW delivery fail");
+}
+
 
 
 void BLDC_HandlerTask(void *pvParameters){
   while(1){
-    digitalWrite(axupin, HIGH);
     BLDC.handler();
-    digitalWrite(axupin, LOW);
     vTaskDelay(1);
   }
 }
@@ -86,7 +147,64 @@ void setup() {
   delay(3000);
   Serial.println("Booted HoverGate V2!!!");
 
-  pinMode(axupin, OUTPUT);
+  //set wifi mode
+  WiFi.mode(WIFI_STA);
+
+  Serial.println("Connecting to Wi-Fi...");
+  unsigned long startConnect = millis();
+  // connecting to WiFi so ESP-NOW is on the same channel
+  WiFi.begin("ssid", "password");
+  // Wait until the connection has been established. 10s timeout
+  while (WiFi.status() != WL_CONNECTED && millis() - startConnect < 10000) {
+    delay(500);
+    Serial.print(".");
+  }
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.println("Failed to connect to Wi-Fi. ESP-NOW will use default channel. Will not retry WiFi connection.");
+    return;
+  }
+  else{
+    //connected, print IP and channel
+    Serial.println("Connected to Wi-Fi");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+    Serial.print("Wi-Fi Channel: ");
+    Serial.println(WiFi.channel());
+  }
+
+  //init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+      Serial.println("Error initializing ESP-NOW");
+      return;
+  }
+  else{
+      Serial.println("ESP-NOW init OK!");
+  }
+
+  //register ESP-NOW callbacks
+  esp_now_register_recv_cb(onEspnowRecv);
+  esp_now_register_send_cb(onEspnowSent);
+
+  // add ESP-NOW peer
+  esp_now_peer_info_t peerInfo;
+  memset(&peerInfo, 0, sizeof(peerInfo)); //all zeros
+  memcpy(peerInfo.peer_addr, send_to_mac_addr, 6); // Ensure receiver_mac is defined
+  peerInfo.channel = 0;  // same as WiFi channel (or default if not on wifi)
+  peerInfo.encrypt = false;
+
+  // add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+      Serial.println("Failed to add peer");
+      return;
+  }
+  else{
+    Serial.println("Peer added");
+  }
+
+
+  
+
+
 
   // gate.set_driver(&BLDC);
   #ifndef GATE_SHORT
@@ -126,10 +244,6 @@ void setup() {
 void loop() {
   static uint32_t n = 0;
   static uint32_t last_t = millis();
-  // put your main code here, to run repeatedly:
-  // digitalWrite(axupin, HIGH);
-  // BLDC.handler();
-  // digitalWrite(axupin, LOW);
 
   // 1 per sec event
   if(millis() - last_t > 1000){
@@ -150,9 +264,6 @@ void loop() {
       BLDC.enable();
       BLDC.set_pwm(0);
       Serial.println("BLDC set to 0");
-
-
-
     }
 
     if(n==5){
